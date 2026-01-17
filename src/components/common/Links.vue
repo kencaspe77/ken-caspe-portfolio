5a01f384c641fa0d2ed9338c041f4c6a058a608d
kencaspe77 <kencaspe77@gmail.com>
2026-01-17 14:55:33 +0800

    Updated nav links

--- src/components/common/Links.vue
+++ src/components/common/Links.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { ref } from "vue";
 import { Mail, Github, Facebook } from "lucide-vue-next";
+
 const navLinks = ref([
   { href: "#", text: "Home" },
-  { href: "#projects", text: "Projects" },
   { href: "#about", text: "About" },
+  { href: "#projects", text: "Projects" },
   { href: "#resume", text: "Resume" },
 ]);
 
@@ -14,29 +15,54 @@ const socialLinks = ref([
   { href: "mailto:kencaspe77@gmail.com", icon: Mail },
 ]);
 </script>
+
 <template>
   <!-- Center Navigation Links -->
   <ul class="flex gap-10">
-    <li v-for="link in navLinks" :key="link.text">
+    <li v-for="link in navLinks" :key="link.text" class="group">
       <a
         :href="link.href"
-        class="hover:text-sky-500 transition-colors flex items-center gap-2"
+        class="nav-links hover:text-purple-300 transition-colors flex items-center gap-2"
       >
-        <component :is="link.icon" :size="20" />
-        <span class="">{{ link.text }}</span>
+        <span class="max-w-0 overflow-hidden whitespace-nowrap">
+          {{ link.text }}
+        </span>
       </a>
     </li>
   </ul>
+
   <!-- Right Social Links -->
   <ul class="flex gap-4">
     <li v-for="link in socialLinks" :key="link.href">
       <a
         :href="link.href"
-        class="hover:text-sky-500 transition-colors flex items-center"
+        target="_blank"
+        class="hover:text-purple-300 transition-colors flex items-center"
       >
         <component :is="link.icon" :size="24" />
       </a>
     </li>
   </ul>
 </template>
-<style scoped></style>
+
+<style scoped>
+.nav-links {
+  position: relative;
+  display: inline-block;
+}
+
+.nav-links::after {
+  content: "";
+  position: absolute;
+  bottom: -2px;
+  left: 0;
+  width: 0;
+  height: 2px;
+  background-color: currentColor;
+  transition: width 0.3s ease-out;
+}
+
+.nav-links:hover::after {
+  width: 100%;
+}
+</style>
